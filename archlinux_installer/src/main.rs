fa78325aa9969059d7956f2363153125830b6045
MadlyJado <jadonchenard@gmail.com>
2023-04-18 18:00:59 -0400

    Added more to archlinux_installer

--- archlinux_installer/src/main.rs
+++ archlinux_installer/src/main.rs
@@ -1,19 +1,32 @@
-use std::io;
 use std::process::Command;
 use std::process::exit;
+use std::io;
 
 
-fn windowsorlinux() -> () {
+fn windowsorlinux() {
     let output = if cfg!(target_os = "windows") {
         println!("You are on windows! This program is for installing arch linux only!");
-        exit(1);
+        exit(0);
     } else {
         println!("Welcome to Jadon's Arch Linux Installer!");
     };
+}
 
-    output
+fn chooseadisk() {
+    let fdisk = Command::new("fdisk")
+    .arg("-l")
+    .output()
+    .expect("failed to execute fdisk command!");
+
+    println!("Which Disk would you like to choose\n {}", String::from_utf8_lossy(&fdisk.stdout));
+
+    println!("Type in the path to the disk you would like to use (ex: /dev/sda): ");
+    let mut diskpath = String::new();
+
+    io::stdin().read_line(&mut diskpath).expect("failed to read dispath input!");
 }
 
 fn main() {
     windowsorlinux();
+    chooseadisk();
 }
